15febdac626e536914a748ec6d9deb0033d854e6
willmeira <willian0283@gmail.com>
2018-09-24 21:46:20 -0300

    exp 01

---
+++ Optativas/CE063C - Sensometria/Script experimentos.R
@@ -0,0 +1,29 @@
+## Experimento 01 - Teste Triangular 
+
+require("SensoMineR")
+require("FactoMineR")
+
+library("SensoMineR")
+library("FactoMineR")
+
+triangle.design(2,24)
+triangle.pair.test(16,24)
+
+
+
+## Experimento 02 - Teste Tetraedrico
+
+require("SensoMineR")
+require("FactoMineR")
+
+library("SensoMineR")
+library("FactoMineR")
+
+triangle.design(2,24)
+triangle.pair.test(16,24)
+
+
+
+delineamento <- triangle.design(2,24)
+resultado <- triangle.pair.test(16,24)
+
